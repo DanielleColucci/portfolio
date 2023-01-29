bc756a38d8c8f8fcfbe0a7521bf578e88b83b5d0
DanielleColucci <ddcolucci99@gmail.com>
2023-01-29 13:13:54 -0500

    stub up basic Projects content

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,6 +1,15 @@
+import { projects } from "../data/projects"
+
 const Projects = () => {
   return (
-    <h1>Projects</h1>
+    <>
+      <h1>Projects</h1>
+      <ul>
+        {projects.map((project, idx) => (
+          <li key={idx}>{project.title}</li>
+        ))}
+      </ul>
+    </>
   )
 }
 
