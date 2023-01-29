4bd2cede79deaf1e6c152424f14c92f13bc2fd74
DanielleColucci <ddcolucci99@gmail.com>
2023-01-29 11:50:37 -0500

    add basic Home page content

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,15 @@
+import '../styles/Home.css'
+
 const Home = () => {
   return (
-    <h1>Home</h1>
+    <>
+      <div className="header">
+        <p>Hi, I am</p>
+        <h1>Danielle Colucci</h1>
+        <p>Research Biologist turned Software Engineer</p>
+      </div>
+      <img src="./assets/headshot.jpeg" alt="profile" />
+    </>
   )
 }
 
