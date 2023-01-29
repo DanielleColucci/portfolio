0724494752d3bae722fdc02d85d89c4ae30a3a5a
DanielleColucci <ddcolucci99@gmail.com>
2023-01-29 12:38:36 -0500

    add basic Resume page content

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,6 +1,9 @@
 const Resume = () => {
   return (
-    <h1>Resume</h1>
+    <>
+      <h1>Resume</h1>
+      <p>Download my <a href="./Danielle-Colucci_Resume.pdf" download="Danielle-Colucci_Resume.pdf">Resume</a></p>
+    </>
   )
 }
 
