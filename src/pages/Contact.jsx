d4e8be76dce1bf1962542b39e6832a76c79af25e
DanielleColucci <ddcolucci99@gmail.com>
2023-01-29 12:03:55 -0500

    add basic Contact page content

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,14 @@
 const Contact = () => {
   return (
-    <h1>Contact</h1>
+    <>
+      <h1>Let's chat!</h1>
+      <h3>Reach out via <a href="mailto:ddcolucci99@gmail.com">email</a> with comments/questions or if you would like to collaborate.</h3>
+      <h4>You can also reach out on socials:</h4>
+      <ul>
+        <li><a href="https://www.linkedin.com/in/daniellecolucci99/" target="_blank" rel="noreferrer">LinkedIn</a></li>
+        <li><a href="https://github.com/DanielleColucci" target="_blank" rel="noreferrer">GitHub</a></li>
+      </ul>
+    </>
   )
 }
 
