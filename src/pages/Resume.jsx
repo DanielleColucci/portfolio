74ae752766efb8915a8f1049b98e5b776a70874c
DanielleColucci <ddcolucci99@gmail.com>
2023-01-31 18:38:08 -0500

    update resume page content and styling

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,10 +1,14 @@
+import File from "../assets/file.png"
 import "../styles/Resume.css"
 
 const Resume = () => {
   return (
     <main id="resume-main">
-      <h1 id="resume-header">Resume</h1>
-      <p>Download my <a href="./Danielle-Colucci_Resume.pdf" download="Danielle-Colucci_Resume.pdf">Resume</a></p>
+      <h1 id="resume-header"><span>Resume</span></h1>
+      <a href="./Danielle-Colucci_Resume.pdf" download="Danielle-Colucci_Resume.pdf">
+        <img id="resume-icon" src={File} alt="resume icon" />
+      </a>
+      <p>Click the icon to download my <span className="bg">resume</span>!</p>
     </main>
   )
 }
