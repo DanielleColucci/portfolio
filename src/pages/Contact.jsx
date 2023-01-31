c74f604855c0bbfee886374e1196875e24663cee
DanielleColucci <ddcolucci99@gmail.com>
2023-01-30 22:10:36 -0500

    update contact page layout, content, and styling

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,14 +1,24 @@
+import '../styles/Contact.css'
+import LinkedInLogo from '../assets/linkedin.png'
+import GitHubLogo from '../assets/github.png'
+
 const Contact = () => {
   return (
-    <>
-      <h1>Let's chat!</h1>
-      <h3>Reach out via <a href="mailto:ddcolucci99@gmail.com">email</a> with comments/questions or if you would like to collaborate.</h3>
-      <h4>You can also reach out on socials:</h4>
-      <ul>
-        <li><a href="https://www.linkedin.com/in/daniellecolucci99/" target="_blank" rel="noreferrer">LinkedIn</a></li>
-        <li><a href="https://github.com/DanielleColucci" target="_blank" rel="noreferrer">GitHub</a></li>
-      </ul>
-    </>
+    <main className='contact-main'>
+      <h1><span>Let's chat!</span></h1>
+      <h3>Reach out with comments/questions or if you would like to collaborate. <br /> The best way to reach me is via <a href="mailto:ddcolucci99@gmail.com"><span className='bg'>email</span></a></h3>
+      <div id='socials-container'>
+        <h2><span>Or reach out on socials:</span></h2>
+        <div>
+          <a href="https://www.linkedin.com/in/daniellecolucci99/" target="_blank" rel="noreferrer">
+            <img className='logo' src={LinkedInLogo} alt="LinkedIn logo" />
+          </a>
+          <a href="https://github.com/DanielleColucci" target="_blank" rel="noreferrer">
+            <img className='logo' src={GitHubLogo} alt="GitHub logo" />
+          </a>
+        </div>
+      </div>
+    </main>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,14 +3,14 @@ import Headshot from '../assets/headshot.jpeg'
 
 const Home = () => {
   return (
-    <main>
+    <main className='home-main'>
       <div id="content-container">
         <div className="header">
           <p><span className='bg'>Hello there, </span>my name is</p>
           <h1><span>Danielle Colucci</span></h1>
           <p>A Boston-based <span className='bg'>software engineer</span> with a passion for problem solving and an eye for detail</p>
         </div>
-        <img src={Headshot} alt="profile" />
+        <img id='headshot' src={Headshot} alt="profile" />
       </div>
     </main>
   )
