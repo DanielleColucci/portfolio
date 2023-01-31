8b2201aa33a473aba4438c5f71e025363209b6a9
DanielleColucci <ddcolucci99@gmail.com>
2023-01-30 22:23:54 -0500

    update projects list and nav bar styling

--- src/components/ProjectsList.jsx
+++ src/components/ProjectsList.jsx
@@ -1,15 +1,16 @@
+import '../styles/ProjectsList.css'
 import ProjectPreview from "./ProjectPreview"
 
 const ProjectsList = (props) => {
   return (
-    <>
-      <h1>Title</h1>
-      <ul>
+    <main id='projects-list-main'>
+      <h1 id='projects-title'><span>Projects</span></h1>
+      <ul id="projects-list">
         {props.projects.map((project, idx) => (
           <ProjectPreview key={idx} project={project} />
         ))}
       </ul>
-    </>
+    </main>
   )
 }
 
