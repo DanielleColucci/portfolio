44fbcc8f5fc32713443cefabe68e41b9e73c2a06
DanielleColucci <ddcolucci99@gmail.com>
2023-01-30 21:29:20 -0500

    update home page styling, layout, and content

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,14 +3,16 @@ import Headshot from '../assets/headshot.jpeg'
 
 const Home = () => {
   return (
-    <>
-      <div className="header">
-        <p>Hi, I am</p>
-        <h1>Danielle Colucci</h1>
-        <p>Research Biologist turned Software Engineer</p>
+    <main>
+      <div id="content-container">
+        <div className="header">
+          <p><span className='bg'>Hello there, </span>my name is</p>
+          <h1><span>Danielle Colucci</span></h1>
+          <p>A Boston-based <span className='bg'>software engineer</span> with a passion for problem solving and an eye for detail</p>
+        </div>
+        <img src={Headshot} alt="profile" />
       </div>
-      <img src={Headshot} alt="profile" />
-    </>
+    </main>
   )
 }
 
