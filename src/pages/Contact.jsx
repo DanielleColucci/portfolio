5bf9ae206ee1235f1afb24b4b260380f94e091ae
DanielleColucci <ddcolucci99@gmail.com>
2023-01-31 19:11:11 -0500

    add email icon to contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,12 +1,18 @@
 import '../styles/Contact.css'
 import LinkedInLogo from '../assets/linkedin.png'
 import GitHubLogo from '../assets/github.png'
+import Email from '../assets/email.png'
 
 const Contact = () => {
   return (
     <main className='contact-main'>
       <h1><span>Let's chat!</span></h1>
-      <h3>Reach out with comments/questions or if you would like to collaborate. <br /> The best way to reach me is via <a href="mailto:ddcolucci99@gmail.com"><span className='bg'>email</span></a></h3>
+      <div id='email-content'>
+        <h3>Reach out with comments/questions or if you would like to collaborate. <br /> The best way to reach me is via <a href="mailto:ddcolucci99@gmail.com"><span className='bg'>email</span></a></h3>
+        <a href="mailto:ddcolucci99@gmail.com">
+          <img src={Email} alt="email icon" />
+        </a>
+      </div>
       <div id='socials-container'>
         <h2><span>Or reach out on socials:</span></h2>
         <div>
